afad75d00a3b610f0308c2cdb281db3ee7d34071
Guillaume Lachance <glachance@cmaisonneuve.qc.ca>
2023-12-02 23:31:26 -0500

    mis-à-jour énoncé TP2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  globals: { "google": "readonly" },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
